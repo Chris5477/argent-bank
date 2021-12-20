d528c914e4883891ee8105401a6fdda079d464be
Chris5477 <petitcolaschristopher@gmail.com>
2021-12-20 15:34:08 +0100

    add responsive design for portal

--- src/components/OnlineNav.js
+++ src/components/OnlineNav.js
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import disconnect from "../img/disconnect.png";
 import Button from "./Button";
 
 const OnlineNav = ({ closeModal }) => {
+	console.log(window.innerWidth);
+	const device = window.innerWidth < 798 ? <img src={disconnect} alt="logged out" /> : "log out";
+
 	return (
 		<>
-			<Link to="/profil">
+			<Link className="account-link" to="/profil">
 				<span className="fa fa-user-circle"></span>
 				My account
 			</Link>
-			<Button method={() => closeModal()} nameClass={"disconnect-button"} text={"log out"} />
+			<Button method={() => closeModal()} nameClass={"disconnect-button"} text={device} />
 		</>
 	);
 };
