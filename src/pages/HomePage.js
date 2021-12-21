c3ea7e2fadfea3013033f74deca98a2529f9e01a
Chris5477 <petitcolaschristopher@gmail.com>
2021-12-21 22:54:46 +0100

    finish file swagger.yaml and add a redirection if user check input remember-me

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";
 import Features from "../components/Features";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 const HomePage = () => {
+	const history = useHistory();
+
+	JSON.parse(localStorage.getItem("token")) && history.push("/profil");
+
 	return (
 		<main>
 			<Header />
